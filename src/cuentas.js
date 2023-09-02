2654491e155993abce25b79a6b04cb01899ca548
Shules <julian.ceballos@hotmail.com>
2023-09-02 15:55:10 -0300

    añado iconos y avanzo con cuentas

---
+++ src/cuentas.js
@@ -0,0 +1,15 @@
+verAlias1 = () => {
+    
+    alert('Su Alias es: arbol.caja.grillo\nSu CBU/CVU es: 000054218158461051');
+}
+
+verAlias2 = () => {
+    
+    alert('Su Alias es: madera.sifon.tormenta\nSu CBU/CVU es: 000026198168457081');
+}
+
+
+verAliasUSD = () => {
+    
+    alert('Su Alias es: risa.amarillo.capa\nSu CBU/CVU es: 000054810625489210');
+}
